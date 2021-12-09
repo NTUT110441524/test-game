2e507a1aebbc1ec61475d02d1f42111116f966e7
NTUT110441524 <95857769+NTUT110441524@users.noreply.github.com>
2021-12-09 21:41:33 +0800

    Update extest.py

--- extest.py
+++ extest.py
@@ -1,196 +1,158 @@
-from PyQt5.QtWidgets import QWidget, QLabel
-from PyQt5.QtCore import QTimer, Qt
-import random
+import sys
+import pygame
+from pygame.locals import *
 
-class Point:
-    pass
+SIZE = 30  # 每個小方格大小
+BLOCK_HEIGHT = 20  # 遊戲區高度
+BLOCK_WIDTH = 10   # 遊戲區寬度
+BORDER_WIDTH = 4   # 遊戲區邊框寬度
+BORDER_COLOR = (40, 40, 200)  # 遊戲區邊框顏色
+SCREEN_WIDTH = SIZE * (BLOCK_WIDTH + 5)  # 遊戲螢幕的寬
+SCREEN_HEIGHT = SIZE * BLOCK_HEIGHT      # 遊戲螢幕的高
+BG_COLOR = (40, 40, 60)  # 背景色
+BLACK = (0, 0, 0)
 
-bonus = Point()
-GAME_ROW = 20
-GAME_COL = 20
-GRID_WIDTH = 20
-GRID_HEIGHT = 20
-SNAKE_INIT_ROW = 10
-SNAKE_INIT_LEMGTH = 5
-SNAKE_Q_LENGTH = GAME_ROW * GAME_COL + 100
-SNAKE_COLOR =      "background-color:#ffffff;"
-SNAKE_HEAD_COLOR = "background-color:#ffff00;"
-BONUS_COLOR      = "background-color:#ff0000;"
-BG_COLOR =         "background-color:#000000;"
-snake_head = SNAKE_INIT_LEMGTH - 1 
-snake_tail = 0
-SNAKE_ADD_LENGTH = 5
-snake_move = 1
 
-class myWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        
-    def keyPressEvent(self, event):
-        global snake_dir        
-        if event.key() == Qt.Key_Up:
-            if snake_dir != 2:
-               snake_dir = 1
-        
-        if event.key() == Qt.Key_Down:
-            if snake_dir != 1:
-               snake_dir = 2
-        
-        if event.key() == Qt.Key_Left:
-            if snake_dir != 4:
-               snake_dir = 3
-    
-        if event.key() == Qt.Key_Right:
-            if snake_dir != 3:
-               snake_dir = 4
-    
-window = myWindow()
+def print_text(screen, font, x, y, text, fcolor=(255, 255, 255)):
+    imgText = font.render(text, True, fcolor)
+    screen.blit(imgText, (x, y))
 
-def inside_snake(mode, x, y):
-    #print('inside_snake():')
-    global snake_q, snake_head, snake_tail, SNAKE_Q_LENGTH
-    s = snake_head 
-    e = snake_tail    
-    if mode == 1:
-        if x == snake_q[s].x and y == snake_q[s].y:
-            #print('inside_snake(1)')
-            return True
-            
-    while True:
-        s -= 1
-        if s == -1:
-            s = SNAKE_Q_LENGTH - 1
-        if x == snake_q[s].x and y == snake_q[s].y:
-            #print('inside_snake(0)')
-            return True
 
-        if s == e:
-            break            
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('俄羅斯方塊')
 
-    return False
+    font1 = pygame.font.SysFont('SimHei', 24)  # 黑體24
+    font_pos_x = BLOCK_WIDTH * SIZE + BORDER_WIDTH + 10  # 右側資訊顯示區域字型位置的X座標
+    font1_height = int(font1.size('得分')[1])
 
-def new_bonus():
-    global bonus, grids, GAME_COL, GAME_ROW, BONUS_COLOR
-    while True:
-        bonus.x = random.randint(0, GAME_COL-1)
-        bonus.y = random.randint(0, GAME_ROW-1)
-        if not inside_snake(1, bonus.x, bonus.y):
-            break
-    #print('new_bonus():', bonus.x, bonus.y) 
-    grids[bonus.y][bonus.x].setStyleSheet(BONUS_COLOR)
-   
-def game_init():
-    global GAME_ROW, GAME_COL, grids, BG_COLOR, SNAKE_INIT_LEMGTH, snake_q, snake_head, snake_tail, snake_dir, game_mode, snake_move, score
-    for i in range(GAME_ROW):
-        for j in range(GAME_COL):
-            grids[i][j].setStyleSheet(BG_COLOR)    
-
-    for i in range(SNAKE_INIT_LEMGTH):
-        snake_q[i].y = SNAKE_INIT_ROW; 
-        snake_q[i].x = i;
-        grids[SNAKE_INIT_ROW][i].setStyleSheet(SNAKE_COLOR)        
-    snake_head = SNAKE_INIT_LEMGTH - 1
-    snake_tail = 0;
-    grids[snake_q[snake_head].y][snake_q[snake_head].x].setStyleSheet(SNAKE_HEAD_COLOR)
+    score = 0           # 得分
 
     while True:
-        snake_dir = random.randint(1, 4)
-        if snake_dir != 3:
-            break
-    game_mode = 1 
-    snake_move = 1 
-    score = 0
-    new_bonus()    
-#---------------------------------  main program ----------------------------------------------
-grids = []
-for y in range(GAME_COL):
-    g_row=[] #for 1 row only
-    for x in range(GAME_ROW):
-        g = QLabel(window)
-        g.setStyleSheet(BG_COLOR) #set all grids with black color
-        g.setFixedSize(GRID_WIDTH, GRID_HEIGHT)
-        g.move(GRID_WIDTH*x, GRID_HEIGHT*y)
-        g_row.append(g)        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+
+        # 填充背景色
+        screen.fill(BG_COLOR)
+        # 畫遊戲區域分隔線
+        pygame.draw.line(screen, BORDER_COLOR,
+                         (SIZE * BLOCK_WIDTH + BORDER_WIDTH // 2, 0),
+                         (SIZE * BLOCK_WIDTH + BORDER_WIDTH // 2, SCREEN_HEIGHT), BORDER_WIDTH)
+        # 畫網格線 豎線
+        for x in range(BLOCK_WIDTH):
+            pygame.draw.line(screen, BLACK, (x * SIZE, 0), (x * SIZE, SCREEN_HEIGHT), 1)
+        # 畫網格線 橫線
+        for y in range(BLOCK_HEIGHT):
+            pygame.draw.line(screen, BLACK, (0, y * SIZE), (BLOCK_WIDTH * SIZE, y * SIZE), 1)
+
+        print_text(screen, font1, font_pos_x, 10, f'得分: ')
+        print_text(screen, font1, font_pos_x, 10 + font1_height + 6, f'{score}')
+        print_text(screen, font1, font_pos_x, 20 + (font1_height + 6) * 2, f'速度: ')
+        print_text(screen, font1, font_pos_x, 20 + (font1_height + 6) * 3, f'{score // 10000}')
+        print_text(screen, font1, font_pos_x, 30 + (font1_height + 6) * 4, f'下一個：')
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
+    ['.0..',
+ '.0..',
+ '.0..',
+ '.0..']
     
-    grids.append(g_row) #create 2-D array of grids
-
-snake_q = []
-for i in range(SNAKE_Q_LENGTH):
-    p = Point()
-    snake_q.append(p)
+    ['....',
+ '....',
+ '0000',
+ '....']
     
-for i in range(SNAKE_INIT_LEMGTH):
-    p = Point()
-    p.y = SNAKE_INIT_ROW
-    p.x = i
-    snake_q[i] = p 
-    grids[SNAKE_INIT_ROW][i].setStyleSheet(SNAKE_COLOR)                
-grids[snake_q[snake_head].y][snake_q[snake_head].x].setStyleSheet(SNAKE_HEAD_COLOR)
+    ['.0..',
+ '.0..',
+ '.0..',
+ '.0..']
+    
+    ['.0.',
+ '000',
+ '...']
+    
+    game_area = [['.'] * BLOCK_WIDTH for _ in range(BLOCK_HEIGHT)]
+    
+cur_block = None   # 當前下落方塊
+cur_pos_x, cur_pos_y = 0, 0  # 當前下落方塊的座標
 
-while True:
-    snake_dir = random.randint(1, 4)
-    if snake_dir != 3:
-        break
-game_mode = 1
-snake_move = 1 
-score = 0
-new_bonus()
-timer = QTimer(window)  
+from collections import namedtuple
 
-def timer_tick():
-    global snake_q, snake_head, SNAKE_Q_LENGTH, GAME_ROW, GAME_COL, timer, SNAKE_ADD_LENGTH, snake_move, score, snake_tail, BONUS_COLOR
-    #print('snake_q len=', len(snake_q), 'snake_head=', snake_head)
-    y = snake_q[snake_head].y
-    x = snake_q[snake_head].x
-    snake_head += 1
-    if snake_head == SNAKE_Q_LENGTH:
-        snake_head = 0    
-    snake_q[snake_head].y = y 
-    snake_q[snake_head].x = x
+Point = namedtuple('Point', 'X Y')
+Block = namedtuple('Block', 'template start_pos end_pos name next')
 
-    if snake_dir == 1:
-        snake_q[snake_head].y -= 1
-        if snake_q[snake_head].y < 0:
-            snake_q[snake_head].y = GAME_ROW - 1    
+# S形方塊
+S_BLOCK = [Block(['.00',
+                  '00.',
+                  '...'], Point(0, 0), Point(2, 1), 'S', 1),
+           Block(['0..',
+                  '00.',
+                  '.0.'], Point(0, 0), Point(1, 2), 'S', 0)]
+BLOCKS = {'O': O_BLOCK,
+          'I': I_BLOCK,
+          'Z': Z_BLOCK,
+          'T': T_BLOCK,
+          'L': L_BLOCK,
+          'S': S_BLOCK,
+          'J': J_BLOCK}
 
-    if snake_dir == 2:
-        snake_q[snake_head].y += 1
-        if snake_q[snake_head].y >= GAME_ROW:
-            snake_q[snake_head].y = 0    
 
-    if snake_dir == 3:
-        snake_q[snake_head].x -= 1
-        if snake_q[snake_head].x < 0:
-            snake_q[snake_head].x = GAME_COL - 1    
+def get_block():
+    block_name = random.choice('OIZTLSJ')
+    b = BLOCKS[block_name]
+    idx = random.randint(0, len(b) - 1)
+    return b[idx]
 
-    if snake_dir == 4:
-        snake_q[snake_head].x += 1
-        if snake_q[snake_head].x >= GAME_COL:
-            snake_q[snake_head].x = 0
-     
-    grids[snake_q[snake_head].y][snake_q[snake_head].x].setStyleSheet(SNAKE_HEAD_COLOR)
-    grids[y][x].setStyleSheet(SNAKE_COLOR)        
-    if inside_snake(1, bonus.x, bonus.y):
-        new_bonus()
-        snake_move += SNAKE_ADD_LENGTH
-        score += 100     
-        print('score=', score)
 
-    if inside_snake(0, snake_q[snake_head].x, snake_q[snake_head].y):
-        print('game over')
-        timer.stop()        
-        game_init()
-        timer.start()
-        return     
+# 獲取旋轉後的方塊
+def get_next_block(block):
+    b = BLOCKS[block.name]
+    return b[block.next]
 
-    if snake_move == 1:
-        grids[snake_q[snake_tail].y][snake_q[snake_tail].x].setStyleSheet(BG_COLOR)
-        snake_tail += 1
-        if snake_tail == SNAKE_Q_LENGTH:
-            snake_tail = 0
+def _judge(pos_x, pos_y, block):
+    nonlocal game_area
+    for _i in range(block.start_pos.Y, block.end_pos.Y + 1):
+        if pos_y + block.end_pos.Y >= BLOCK_HEIGHT:
+            return False
+        for _j in range(block.start_pos.X, block.end_pos.X + 1):
+            if pos_y + _i >= 0 and block.template[_i][_j] != '.' and game_area[pos_y + _i][pos_x + _j] != '.':
+                return False
+    return True
+
+def _dock():
+    nonlocal cur_block, next_block, game_area, cur_pos_x, cur_pos_y, game_over
+    for _i in range(cur_block.start_pos.Y, cur_block.end_pos.Y + 1):
+        for _j in range(cur_block.start_pos.X, cur_block.end_pos.X + 1):
+            if cur_block.template[_i][_j] != '.':
+                game_area[cur_pos_y + _i][cur_pos_x + _j] = '0'
+    if cur_pos_y + cur_block.start_pos.Y <= 0:
+        game_over = True
     else:
-        snake_move -= 1
+        # 計算消除
+        remove_idxs = []
+        for _i in range(cur_block.start_pos.Y, cur_block.end_pos.Y + 1):
+            if all(_x == '0' for _x in game_area[cur_pos_y + _i]):
+                remove_idxs.append(cur_pos_y + _i)
+        if remove_idxs:
+            # 消除
+            _i = _j = remove_idxs[-1]
+            while _i >= 0:
+                while _j in remove_idxs:
+                    _j -= 1
+                if _j < 0:
+                    game_area[_i] = ['.'] * BLOCK_WIDTH
+                else:
+                    game_area[_i] = game_area[_j]
+                _i -= 1
+                _j -= 1
+        cur_block = next_block
+        next_block = blocks.get_block()
+        cur_pos_x, cur_pos_y = (BLOCK_WIDTH - cur_block.end_pos.X - 1) // 2, -1 - cur_block.end_pos.Y
         
-window.show()
-timer.timeout.connect(timer_tick)
-timer.start(200)
+        
